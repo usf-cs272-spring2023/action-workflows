0443b9e61aeb4fcd537b5feb0e9c53264b95bc31
Sophie Engle <sjengle@cs.usfca.edu>
2022-08-31 11:11:41 -0700

    Reworked logic

--- scripts/check-event-labeled.js
+++ scripts/check-event-labeled.js
@@ -3,128 +3,99 @@ module.exports = async ({github, context, core}) => {
   // users that are allowed to edit labels or assignees
   const allowed = new Set(['sjengle', 'mtquach2', 'par5ul1', 'igentle292']);
 
-  // placeholder to store error messages
-  const error_messages = [];
+  // get event information
+  const action = context.payload.action;
+  const sender = context.payload.sender.login;
+  const output = `Action: ${action}, Sender: ${sender}\n`;
 
+  // return if allowed
+  if (allowed.has(sender)) {
+    core.info(`✅ ${output}`);
+    return;
+  }
+
+  // indicate this check failed
+  core.setFailed(`❌ ${output}`);
+
+  // store error messages
+  const error_messages = [`Only approved users may modify issue labels and assignees!`];
+
+  // common params used for requests
   const params = {
     owner: context.payload.organization.login,
     repo: context.payload.repository.name,
     issue_number: context.payload.issue.number
   };
 
-  core.info(JSON.stringify(params, null, "  "));
-  core.info('');
+  core.startGroup(`Undoing ${action} action...`);
+  core.info(JSON.stringify(params));
+  core.endGroup();
 
   try {
-    // get event information
-    const action = context.payload.action;
-    const sender = context.payload.sender.login;
+    // try to undo the change
+    let added = undefined;
+    let response = undefined;
 
-    if (!allowed.users.has(sender)) {
-      error_messages.push(`Only approved users may modify issue labels and assignees!`);
-      core.setGroup(`❌ Action: ${action}, Sender: ${sender}`);
-
-      switch (action) {
-        case 'labeled':
-          const label = context.payload.label.name;
-          const removeLabel = await github.rest.issues.removeLabel({...params, name: label});
-
-          break;
-
-        case 'assigned':
-          const assignee = context.payload.assignee.login;
-          const removeAssignee = await github.rest.issues.removeAssignees({...params, assignees: assignee});
-          break;
-
-        default:
-          error_messages.push(`Unexpected event type: \`${action}\``);
-      }
-      
-      core.endGroup();
-    }
-    else {
-      core.info(`✅ Action: ${action}, Sender: ${sender}`);
-    }
-
-    // check if valid event type
     switch (action) {
       case 'labeled':
-        // check if valid event sender
-        if (!allowed.users.has(sender)) {
-          error_messages.push(`Only approved users may modify issue labels!`);
-
-          core.setGroup(`❌ Action: ${action}, Sender: ${sender}`);
-          const response = await github.rest.issues.removeAllLabels(params);
-          core.info(JSON.stringify(response));
-          core.endGroup();
-        }
-        else {
-          core.info(`✅ Action: ${action}, Sender: ${sender}`);
-        }
+        added = context.payload.label.name;
+        response = await github.rest.issues.removeLabel({...params, name: added});
         break;
 
       case 'assigned':
-        // check if valid event sender
-        if (!allowed.users.has(sender)) {
-          error_messages.push(`Only approved users may modify issue assignees!`);
-         
-          core.setGroup(`❌ Action: ${action}, Sender: ${sender}`);
-          const assignees = context.payload.issue.assignees.map(x => x.login);
-          const response = await github.rest.issues.removeAssignees({...params, assignees: assignees});
-          core.info(JSON.stringify(response));
-          core.endGroup();
-        }
-        else {
-          core.info(`✅ Action: ${action}, Sender: ${sender}`);
-        }
+        added = context.payload.assignee.login;
+        response = await github.rest.issues.removeAssignees({...params, assignees: added});
         break;
 
       default:
         error_messages.push(`Unexpected event type: \`${action}\``);
-        core.info(`❌ Action: ${action}, Sender: ${sender}`);
+    }
+
+    // if make it this far, report result
+    if (response != undefined && response.status === 200) {
+      core.info(`Removed ${added} from issue (status: ${response.status}).`);
+    }
+    else {
+      error_messages.push(`Unable to remove ${added}.`);
+      core.startGroup(`Failed to undo ${action} action...`);
+      core.info(JSON.stringify(response));
+      core.endGroup();
     }
   }
-  catch (error) {
+  catch(error) {
+    // add error and output stack trace
     error_messages.push(`Unexpected error: ${error.message}`);
-    core.startGroup(`❌ Unexpected Error: ${error.message}`);
+    core.startGroup(`Unexpected ${error.name} encountered...`);
     console.trace(error);
     core.endGroup(``);
   }
   finally {
-    if (error_messages.length === 0) {
-      core.info(`✅ Found ${error_messages.length} problem(s) with this issue.`);
+    core.startGroup(`Outputting errors...`);
+    for (const message of error_messages) {
+      core.error(message);
     }
-    else {
-      core.setFailed(`❌ Found ${error_messages.length} problem(s) with this issue.`);
-      core.info('');
+    core.endGroup();
 
-      core.startGroup(`Outputting context...`);
-      core.info(JSON.stringify(context, null, "  "));
-      core.endGroup();
+    core.startGroup(`Outputting context...`);
+    core.info(JSON.stringify(context));
+    core.endGroup();
 
-      core.startGroup(`Outputting errors...`);
-      for (const message of error_messages) {
-        core.error(message);
-      }
-      core.endGroup();
-
-      const formatted = error_messages.map(x => `  1. ${x}`);
-      const issue_body = `@${context.actor} there are ${error_messages.length} problem(s) with your issue:
+    // create issue_body
+    const formatted = error_messages.map(x => `  1. ${x}`);
+    const issue_body = `@${context.sender} there are ${error_messages.length} problem(s) with your ${action} action:
 
 ${formatted.join('\n')}
 
 :octocat: See [run id ${context.runId}](https://github.com/${context.payload.repository.full_name}/actions/runs/${context.runId}) for details.
-      `;
-
-      // attempt to modify issue
-      Promise.allSettled([
-        github.rest.issues.update({...params, labels: ['error'], assignees: [context.actor], state: 'closed'}),
-        github.rest.issues.createComment({...params, body: issue_body})
-      ]).then((results) => {
-        core.startGroup(`Outputting status...`);
-        core.info(JSON.stringify(results));
-        core.endGroup();
-      });
-    }
+    `;
+    
+    // attempt to comment on issue
+    Promise.allSettled([
+      github.rest.issues.createComment({...params, body: issue_body})
+    ]).then((results) => {
+      core.startGroup(`Outputting status...`);
+      core.info(JSON.stringify(results));
+      core.endGroup();
+    });
   }
 };
