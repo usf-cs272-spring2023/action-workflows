20f90ed45b1fd94985bfd3a13fa0c0653d6a43b7
Sophie Engle <sjengle@cs.usfca.edu>
2022-08-31 17:18:19 -0700

    Cleaning up

--- scripts/parse-version.js
+++ scripts/parse-version.js
@@ -11,11 +11,8 @@ module.exports = async ({github, context, core}) => {
       break;
     default:
       const message = `Unexpected event type for parsing release: ${context.eventName}`;
-      core.exportVariable(`ERROR_MESSAGES`, `${message}\n${process.env.ERROR_MESSAGES}`);
-      core.setFailed(`message`);
-      core.startGroup(`Outputting context...`);
-      core.info(JSON.stringify(context));
-      core.endGroup();
+      core.setFailed(message);
+      core.setOutput('ERROR_MESSAGES', message);
       return;
   }
 
@@ -28,8 +25,8 @@ module.exports = async ({github, context, core}) => {
   // cannot continue without a parsable version number
   if (matched === null || matched.length !== 4) {
     const message = `Unable to parse "${release_ref}" into major, minor, and patch version numbers. If a release was made in error, delete the release *and* tag (2 separate steps).`; 
-    core.exportVariable(`ERROR_MESSAGES`, `${message}\n${process.env.ERROR_MESSAGES}`);
     core.setFailed(message);
+    core.setOutput('ERROR_MESSAGES', message);
     return;
   }
 
