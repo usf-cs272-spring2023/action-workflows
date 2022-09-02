2f1703849f03a1a9a37f051b7fc344f68ac245bd
Sophie Engle <sjengle@cs.usfca.edu>
2022-09-01 22:40:31 -0700

    Debugging

--- .github/scripts/run-exec.js
+++ .github/scripts/run-exec.js
@@ -3,18 +3,11 @@ module.exports = async ({exec, core}) => {
   const command = 'grep'
   const args = ['-rnoE', '--exclude=Driver.java', '\\s*public\\s+static\\s+void\\s+main\\s*\\('];
   const cwd = `${process.env.USER_PATH}/src/main/java`;
-  const message = `Except for Driver.java, you should delete old main methods from your code.`;
+  const message = `Except for Driver.java, you should delete old main methods from your code. See the run logs for details.`;
   const options = {ignoreReturnCode: true, cwd: cwd};
-
-
-
-  core.info('potato');
   const result = await exec.exec(command, args, options);
 
-  core.info('pancake');
-
-  if (result !== 0) {
-    core.info(result);
+  if (result !== 1) {
     core.setFailed(message);
   }
 };
