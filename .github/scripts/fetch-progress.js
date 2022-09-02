c94ce5f31556b5aa4dd6e52311e369fd7ff5ab15
Sophie Engle <sjengle@cs.usfca.edu>
2022-09-01 20:15:32 -0700

    Starting on version checks

---
+++ .github/scripts/fetch-progress.js
@@ -0,0 +1,6 @@
+// gets the status of projects from the releases, issues, and pull requests
+module.exports = async ({github, context, core}) => {
+  core.info(JSON.stringify(context, null, "  "));
+
+  
+};
