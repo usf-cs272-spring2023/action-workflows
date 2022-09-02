63360fa896f81f6705d9db66c4f64c3d28200d97
Sophie Engle <sjengle@cs.usfca.edu>
2022-09-01 22:33:03 -0700

    Debugging

--- .github/scripts/run-exec.js
+++ .github/scripts/run-exec.js
@@ -1,26 +1,25 @@
 // nicer exec output than running commands in bash shell
 module.exports = async ({exec, core}) => {
   const command = 'grep'
-  const args = ['-rnoE', '--exclude=Driver.java', "'\\s*public\\s+static\\s+void\\s+main\\s*\\('"];
+  const args = ['-rnoE', '--exclude=Driver.java', '\\s*public\\s+static\\s+void\\s+main\\s*\\('];
   const cwd = `${process.env.USER_PATH}/src/main/java`;
   const message = `Except for Driver.java, you should delete old main methods from your code.`;
 
   const options = {ignoreReturnCode: true, cwd: cwd, failOnStdErr: false};
 
-  let output = '';
+  let output = [];
   let errors = '';
 
   options.listeners = {
     stdout: (data) => {
-      output += Buffer.from(data).toString('utf8');
-      core.info('muffin');
+      output.push(data.toString());
     },
     stderr: (data) => {
-      errors += Buffer.from(data).toString('utf8');
+      errors.push(data.toString());
     }
   };
 
-  core.info('pizza');
+  core.info('potato');
   const result = await exec.exec(command, args, options);
 
   core.info(output);
