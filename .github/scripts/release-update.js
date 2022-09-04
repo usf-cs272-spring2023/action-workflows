79602a5d09f5bbb4e8ddc5d46dd6e9c1dcc8b9c9
Sophie Engle <sjengle@cs.usfca.edu>
2022-09-03 18:33:20 -0700

    Parsing

--- .github/scripts/release-update.js
+++ .github/scripts/release-update.js
@@ -1,8 +1,9 @@
 // checks the status from previous jobs and updates the release
 module.exports = async ({github, context, core}) => {
   const results = process.env.RESULTS;
-
   core.info(results);
-
+  
+  const json = JSON.parse(results);
+  core.info(json);
 
 };
