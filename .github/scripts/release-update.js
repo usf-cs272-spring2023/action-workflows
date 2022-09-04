a16388e7c8435762b610bb74fdb293a99c072829
Sophie Engle <sjengle@cs.usfca.edu>
2022-09-03 18:54:24 -0700

    Adding try/catch

--- .github/scripts/release-update.js
+++ .github/scripts/release-update.js
@@ -1,27 +1,30 @@
 // checks the status from previous jobs and updates the release
 module.exports = async ({github, context, core}) => {
-  const results = process.env.RESULTS;
-  const json = JSON.parse(results);
+  try {
+    const results = process.env.RESULTS;
+    const json = JSON.parse(results);
 
-  for (const property in json) {
-    if (json[property].hasOwnProperty('outputs')) {
-      if (json[property]['outputs'].hasOwnProperty('status')) {
-        const parsed = JSON.parse(json[property]['outputs']['status']);
-        json[property]['outputs']['status'] = parsed;
+    for (const property in json) {
+      if (json[property].hasOwnProperty('outputs')) {
+        if (json[property]['outputs'].hasOwnProperty('status')) {
+          const parsed = JSON.parse(json[property]['outputs']['status']);
+          json[property]['outputs']['status'] = parsed;
+        }
       }
     }
+
+    core.info(JSON.stringify(json, null, '  '));
+
+
+    const titles = [
+      'Request Project Design Grade',
+      'Request Project Review Grade',
+      'Request Project Tests Grade',
+      'Request Project Code Review'
+    ];
   }
-
-  core.info(JSON.stringify(json, null, '  '));
-
-
-  const titles = [
-    'Request Project Design Grade',
-    'Request Project Review Grade',
-    'Request Project Tests Grade',
-    'Request Project Code Review'
-  ];
-
-
-
+  catch (error) {
+    core.error(`${error.name}: ${error.message}`);
+    core.setFailed(`Unable to update status of this release.`);
+  }
 };
