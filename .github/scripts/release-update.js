ef5fa52744e998e8e9eb8552909cc6bcb987a4dd
Sophie Engle <sjengle@cs.usfca.edu>
2022-09-03 18:44:59 -0700

    Parsing status

--- .github/scripts/release-update.js
+++ .github/scripts/release-update.js
@@ -1,11 +1,26 @@
 // checks the status from previous jobs and updates the release
 module.exports = async ({github, context, core}) => {
   const results = process.env.RESULTS;
-  core.info(results);
-  core.info('');
-  
   const json = JSON.parse(results);
-  core.info(JSON.stringify(json, null, "  "));
+
+  for (const property in json) {
+    if (json[property].hasOwnProperty('outputs')) {
+      if (json[property]['outputs'].hasOwnProperty('status')) {
+        const parsed = JSON.parse(json[property]['outputs']['status']);
+        json[property]['outputs']['status'] = parsed;
+      }
+    }
+  }
+
+  core.info(JSON.stringify(json, null, '  '));
+
+
+  const titles = [
+    'Request Project Design Grade',
+    'Request Project Review Grade',
+    'Request Project Tests Grade',
+    'Request Project Code Review'
+  ];
 
 
 
