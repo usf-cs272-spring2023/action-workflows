164b44db1cb24daaec5d51a93e533a1b584ded48
Sophie Engle <sjengle@cs.usfca.edu>
2022-09-04 15:43:48 -0700

    Closing issue

--- .github/scripts/issue-results.js
+++ .github/scripts/issue-results.js
@@ -4,14 +4,33 @@ module.exports = async ({github, context, core}) => {
   const message = `:octocat: Request complete. See [run #${context.runNumber} (id ${context.runId})](https://github.com/${context.repo.owner}/${context.repo.repo}/actions/runs/${context.runId}) for details.`;
 
   try {
-    const response = await github.rest.issues.updateComment({
+    const updated = await github.rest.issues.updateComment({
       owner: context.repo.owner,
       repo: context.repo.repo,
       comment_id: comment_id,
       body: message
     });
 
-    core.info(`Updated comment id ${response.data.id}.`);
+    core.info(`Updated comment id ${updated.data.id}.`);
+
+    const labels = []; // TODO
+    const assignees = []; // TODO
+    const milestone = undefined; // TODO
+
+    let reason = "not_planned"; // TODO or "completed"
+
+    const closed = await github.rest.issues.update({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: context.payload.issue.number,
+      labels: labels,
+      assignees: assignees,
+      state: 'closed',
+      state_reason: reason
+    });
+
+    // TODO Check if should close milestone
+
   }
   catch (error) {
     core.info(`${error.name}: ${error.message}`);
