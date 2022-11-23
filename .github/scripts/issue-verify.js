52408de659c39ba0f45b87866c72e30d795e8293
Sophie Engle <sjengle@cs.usfca.edu>
2022-11-23 14:26:09 -0800

    Updating section 3 logic

--- .github/scripts/issue-verify.js
+++ .github/scripts/issue-verify.js
@@ -16,8 +16,12 @@ module.exports = async ({github, context, core}) => {
     const minor = parseInt(process.env.VERSION_MINOR);
     const patch = parseInt(process.env.VERSION_PATCH);
 
+    // check if this is coming from section 3
     const section3 = context.repo.owner.includes('usf-cs272-03-fall2022');
-    core.info(`Detected section 3: ${context.repo.owner}`);
+    
+    if (section3) {
+      core.info(`Detected section 3: ${context.repo.owner}`);
+    }
 
     // list of github usernames that can approve PRs
     const approvers = new Set(['ryscheng', 'cardi', 'sjengle']);
